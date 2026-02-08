047e3c08b5215598f03bac5f074b2cda8fe2aeb4
Sadman Sakib <sadmansakib.wd@gmail.com>
2026-02-08 22:48:44 +0600

    BUeT

--- src/main/java/com/example/project/HelloController.java
+++ src/main/java/com/example/project/HelloController.java
@@ -1,14 +1,21 @@
 package com.example.project;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
+    public Button SadmanButton;
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void onclickSadman(ActionEvent actionEvent) {
+        
     }
 }
